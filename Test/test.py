bbcc5c3b19c91c5954bd36c779a519a2fbd0c9c3
Niqoo10 <nicopanella2009@hotmail.com>
2023-08-23 11:33:15 -0300

    develop

--- Test/test.py
+++ Test/test.py
@@ -1,9 +1,12 @@
 import unittest
+from App.app import multiplicacion
 
 class TestNumber(unittest.TestCase):
 
     def test_mult(self):
-        self.assertEqual(multiplicacion(2, 3,), 6)
+        self.assertEqual(multiplicacion(2, 3), 
+                         6,
+                         )
 
 if __name__ == '__main__':
     unittest.main()
